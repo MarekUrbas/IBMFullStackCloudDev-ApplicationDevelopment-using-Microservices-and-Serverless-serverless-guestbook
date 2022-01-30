59fc50e995e94bc80ca44a433cc126aa6f46012b
Marek Urbas, Promax <83548156+MarekUrbas@users.noreply.github.com>
2022-01-31 00:36:01 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${https://github.com/MarekUrbas/IBMFullStackCloudDev-ApplicationDevelopment-using-Microservices-and-Serverless-serverless-guestbook}/entries`,
       dataType: 'json'
     });
   },
